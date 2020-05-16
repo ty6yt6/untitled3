d2fde961d5df7721a718b11deb21a2bf0cd90534
ty6yt6 <zjw920815@gmail.com>
2020-05-16 11:20:45 +0800

    类视图实现注册页面同时有GET/POST

--- ty6yt6/views.py
+++ ty6yt6/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django import http
+from django.views import View
+# def register(request):
+#     return http.HttpResponse("你看我像不像注册")
+# # Create your views here.
+#
+# def tupi(request):
+#     return http.HttpResponse("wuliao")
 
-def register(request):
-    return http.HttpResponse("你看我像不像注册")
-# Create your views here.
+# 定义类视图
+# class 类名(View):
+#   请求方法可以是POST/GET等等
+#   def 跟请求方法同名的函数名(self,request):
+#         return 响应对象
 
-def tupi(request):
-    return http.HttpResponse("wuliao")
+#
+class RegisterView(View):
+    def get(self, request):
+        return http.HttpResponse("返回注册页面")
+
+    def post(self, request):
+        return http.HttpResponse("实现注册逻辑")
