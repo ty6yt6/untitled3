0faf3b7633c6c6e896b05f9a1bac1f9d829e0e43
ty6yt6 <zjw920815@gmail.com>
2020-05-20 10:43:05 +0800

    增删改查 改-save()

--- booktest/views.py
+++ booktest/views.py
@@ -25,11 +25,21 @@ class TestModelView1(View):
         # creat()方法
         # 语法：模型类.模型类管理器.creat（模型属性=值）
         # 模型管理器：是由Django提供并封装的一个对象（object），用语调用ORM的接口方法，固定的语法
-        BookInfo.objects.create(
-            btitle = "三国演义",
-            bpub_date = "2001-12-12",
-            bread = 100,
-            bcomment = 222,
-        )
+        # BookInfo.objects.create(
+        #     btitle = "三国演义",
+        #     bpub_date = "2001-12-12",
+        #     bread = 100,
+        #     bcomment = 222,
+        # )
+
+        # 新增：save(),create()
+        # save()
+            # 查询出要修改的记录/模型对象
+        book = BookInfo.objects.get(id=5)
+        book.btitle = "西游你改不改记"
+        book.save()
+            # 新值覆盖旧值
+            # 同步新值
+
         return http.HttpResponse("CHENGGONG")
 
