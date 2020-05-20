2b1e3f8689348a542643dcbcbb0deba03464467b
ty6yt6 <zjw920815@gmail.com>
2020-05-20 09:07:25 +0800

    加了admin，页面可以访问admin

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ["id","btitle","bread","bcomment"]
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ["id","hbook","hname","hgender","hcomment"]
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo,HeroInfoAdmin)
